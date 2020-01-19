54cea8e189610c5bab0c8b6886553d40175a4b2f
AhmedBakr <59182132+DEV-Ahmedbakr@users.noreply.github.com>
2020-01-20 01:38:03 +0200

    Router

---
+++ App.js
@@ -0,0 +1,23 @@
+import React,{Component}       from 'react';                      // React to use render() &  Component
+import {BrowserRouter, Route}  from 'react-router-dom';           // BrowserRouter &  Route
+import Navbar                  from './component/Navbar';         // Navbar component 
+import Home                    from './component/Home';           // Home component 
+import Login                   from './component/Login';          // Login component 
+import Data                    from './component/Data';           // Data component 
+import Notfound                from './component/Notfound';       // Notfound component 
+import 'bootstrap/dist/css/bootstrap.min.css';                    // bootstrap  by npm {npm install react-bootstrap bootstrap}
+class App extends Component {
+ render(){
+   return(
+     <BrowserRouter>
+      <Navbar />
+      <Route exact path="/"         component={Home}/>
+      <Route       path="/Login"    component={Login}/>
+      <Route       path="/Data"     component={Data}/>
+      <Route       path="/Notfound" component={Notfound}/>
+     </BrowserRouter>
+   )
+ }
+
+}
+export default App;
