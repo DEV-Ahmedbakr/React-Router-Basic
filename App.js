9d2ea385dfe02ee6029aa72780724b0a6accf689
AhmedBakr <59182132+DEV-Ahmedbakr@users.noreply.github.com>
2020-01-21 13:16:42 +0200

    Update App.js

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ class App extends Component {
       <Route exact path="/"         component={Home}/>
       <Route       path="/Login"    component={Login}/>
       <Route       path="/Data"     component={Data}/>
-      <Route       path="/Notfound" component={Notfound}/>
+      <Route       path="/:Notfound" component={Notfound}/>
      </BrowserRouter>
    )
  }
